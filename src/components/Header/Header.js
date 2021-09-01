52421fc5b60a8a2b04e09a664b6ae90f9e5f78be
Aminul Islam <aminulislamroqib@gmail.com>
2021-09-01 21:50:19 +0600

    social

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@ import { DiCodrops, DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
-          <DiCodrops size="3rem"/><Span>Umar Khosru</Span>
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
+          <DiCodrops size="3rem" /><Span>Umar Khosru</Span>
         </a>
       </Link>
     </Div1>
@@ -37,22 +37,22 @@ const Header = () =>  (
       </li>
       <li>
         <Link href=""  >
-          <NavLink onClick={ () => alert("Umar Khosru Shourav's Blog is coming up...")}>Blog</NavLink>
+          <NavLink onClick={() => alert("Umar Khosru Shourav's Blog is coming up...")}>Blog</NavLink>
         </Link>
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="/">
-        <AiFillInstagram size="3rem"/>
+      <SocialIcons target='_blank' href="https://www.facebook.com/profile.php?id=100053965721045">
+        <AiFillFacebook size="3rem" />
       </SocialIcons>
       <SocialIcons href="/">
-        <AiFillTwitterSquare size="3rem"/>
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="/">
+        <AiFillTwitterSquare size="3rem" />
       </SocialIcons>
       <SocialIcons href="https://linkedin.com">
-        <AiFillLinkedin size="3rem"/>
-      </SocialIcons>
-      <SocialIcons target='_blank' href="https://www.facebook.com/profile.php?id=100053965721045">
-        <AiFillFacebook size="3rem"/>
+        <AiFillLinkedin size="3rem" />
       </SocialIcons>
     </Div3>
   </Container>
